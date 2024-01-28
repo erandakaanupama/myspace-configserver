6901fccb518216a15e0f78229bdaae85f5cd1f1c
erandaka.bandara <erandakaanupama@gmail.com>
2024-01-28 21:37:19 +0530

    feat:initial commit

---
+++ src/main/java/com/eab/myspaceconfigserver/MyspaceConfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.eab.myspaceconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspaceConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspaceConfigserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eab/myspaceconfigserver/MyspaceConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eab.myspaceconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspaceConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
